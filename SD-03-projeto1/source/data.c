7dacbceef37ffce3c1b0a31a59da33c9cc27a16d
Tiago Leite <tiagobscleite@gmail.com>
2025-09-16 12:51:26 +0100

    data.c done

--- SD-03-projeto1/source/data.c
+++ SD-03-projeto1/source/data.c
@@ -4,3 +4,49 @@
  * Simão Alexandre - 61874
  * Tiago Leite - 61863
  */
+
+ #include "../include/data.h"
+
+ struct data_t *data_create(int ano, float preco, enum marca_t marca, const char *modelo, enum combustivel_t combustivel)
+{
+    struct data_t *data = malloc(sizeof(struct data_t));
+    if (!data) return NULL;
+
+    data->ano = ano;
+    data->preco = preco;
+    data->marca = marca;
+    data->modelo = strdup(modelo);
+    data->combustivel = combustivel;
+
+    return data;
+}
+
+int data_destroy(struct data_t *data)
+{
+    if (!data) return -1;
+
+    free(data->modelo);
+    free(data);
+    return 0;
+}
+
+struct data_t *data_dup(struct data_t *data)
+{
+    if (!data) return NULL;
+    return data_create(data->ano, data->preco, data->marca, data->modelo, data->combustivel);
+}
+
+int data_replace(struct data_t *data, int ano, float preco, enum marca_t marca, const char *modelo, enum combustivel_t
+combustivel)
+{
+    if (!data) return -1;
+
+    data->ano = ano;
+    data->preco = preco;
+    data->marca = marca;
+    free(data->modelo);
+    data->modelo = strdup(modelo);
+    data->combustivel = combustivel;
+
+    return 0;
+}
